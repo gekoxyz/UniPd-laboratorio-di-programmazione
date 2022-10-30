8dd4baead6ac959e6e1f94ce013cfa73abd14e35
Matteo Galiazzo <mattegaliz@gmail.com>
2022-10-30 17:58:51 +0100

    finished lab2

--- lab2/rational.cpp
+++ lab2/rational.cpp
@@ -34,6 +34,10 @@ class rational {
     return rational(numerator, denominator);
   }
 
+  rational operator+(int n) {
+    return *this + rational(n);
+  }
+
   rational operator-(const rational& r) {
     // a/b + c/d = (a*d - c*b) / b*d 
     int numerator = this->numerator * r.denominator - this->denominator * r.numerator;
@@ -50,24 +54,24 @@ class rational {
 
   rational operator/(const rational& r) {
     // (a/b) / (c/d) = a/b * d/c
-    return rational(this->numerator, this->denominator) * rational(r.denominator, r.numerator);
+    return *this * rational(r.denominator, r.numerator);
   }
 
   bool operator==(const rational &r) {
-    double num1 = fraction_to_double(rational(this->numerator, this->denominator));
-    double num2 = fraction_to_double(r);
+    double num1 = to_double(*this);
+    double num2 = to_double(r);
     return num1 == num2;
   }
 
   bool operator<(const rational& r) {
-    double num1 = fraction_to_double(rational(this->numerator, this->denominator));
-    double num2 = fraction_to_double(r);
+    double num1 = to_double(*this);
+    double num2 = to_double(r);
     return num1 < num2;
   }
 
   bool operator>(const rational& r) {
-    double num1 = fraction_to_double(rational(this->numerator, this->denominator));
-    double num2 = fraction_to_double(r);
+    double num1 = to_double(*this);
+    double num2 = to_double(r);
     return num1 > num2;
   }
 
@@ -81,27 +85,34 @@ class rational {
 
   private:
 
-  double fraction_to_double(const rational& r) {
+  double to_double(const rational& r) {
     return (double)r.numerator / (double)r.numerator;
   }
   class Invalid {};
 };
+
+ostream& operator<<(ostream& os, rational& r) {
+  return os << r.get_numerator() << "/" << r.get_denominator() << "\n";
+}
 
 int main() {
   rational n1(2,3); 
   rational n2(5,6);
 
   rational res = n1 + n2;
-  // cout << res;
+  cout << res;
+
+  res = n1 + 1;
+  cout << res;
 
   res = n1 - n2;
-  cout << res.get_numerator() << "/" << res.get_denominator() << "\n";
+  cout << res;
 
   res = n1 * n2;
-  cout << res.get_numerator() << "/" << res.get_denominator() << "\n";
+  cout << res;
   
   res = n1 / n2;
-  cout << res.get_numerator() << "/" << res.get_denominator() << "\n";
+  cout << res;
 
   cout << (rational(1, 2) == rational(2, 4)) << "\n";
 
