bc5c024dcda21de6a981dc77fba08e9da6f3235c
Matteo Galiazzo <mattegaliz@gmail.com>
2022-12-12 11:18:13 +0100

    refactored maze::possible_moves

--- lab7/include/maze.h
+++ lab7/include/maze.h
@@ -10,23 +10,26 @@
 
 #include <string>
 #include <vector>
+#include <algorithm>
 
 class maze {
     public:
     // maze.txt is the default maze, specify a new maze if you want a new maze
     maze(std::string filename = "./../assets/maze.txt");
     std::vector<position> possible_moves();
-    void move(position p) { current_ = p; } // TODO: CHECK THAT P IS AN ELEMENT OF possible_moves()
+    bool cell_reachable(char cell);
+    void move(position p);
     void print_maze();
     bool finished() { return current_ == end_; }
     position current() { return current_; }
-
 private:
     std::string filename_;
     std::vector<std::vector<char>> maze_;
     position start_;
     position end_;
     position current_;
+    int row_size_;
+    int col_size_;
 };
 
 #include "maze.hpp"
--- lab7/include/maze.hpp
+++ lab7/include/maze.hpp
@@ -24,8 +24,10 @@ maze::maze(std::string filename) {
         maze_.push_back(line_container);
         row_index++;
     }
-    current_ = start_;
     maze_reader.close();
+    current_ = start_;
+    row_size_ = maze_.size();
+    col_size_ = maze_[0].size();
     print_maze();
 }
 
@@ -34,77 +36,47 @@ std::vector<position> maze::possible_moves() {
     std::vector<position> possible_moves;
     int row = current_.x(); // the x position is the row index
     int col = current_.y(); // the y position is the col index 
+    
     // i need to check if right is a good position (NOT OUT OF BOUNDS and reachable)
     // currently the only obstacle is the wall (*) , so the algorithm just checks
     // that you are not going into a wall
 
-    // TODO: IMPLEMENT IN A GENERIC WAY
+    if (col + 1 <= col_size_ && cell_reachable(maze_[row][col + 1]))
+        possible_moves.push_back(position(row, col + 1)); // next column same row -> right
 
-    if (col + 1 <= maze_[row].size()) {
-        char cell = maze_[row][col + 1]; // next column same row -> right
-        if (cell != '*') {
-            position right(row, col + 1);
-            possible_moves.push_back(right);
-        }
-    }
+    if ((row - 1 >= 0 && col + 1 <= col_size_) && cell_reachable(maze_[row - 1][col + 1]))
+        possible_moves.push_back(position(row - 1, col + 1)); // prev row next column -> top right
 
-    if (row - 1 >= 0 && col + 1 <= maze_[row].size()) {
-        char cell = maze_[row - 1][col + 1]; // prev row next column -> top right
-        if (cell != '*') {
-            position top_right(row - 1, col + 1);
-            possible_moves.push_back(top_right);
-        }
-    }
+    if ((row + 1 <= row_size_ && col + 1 <= col_size_) && cell_reachable(maze_[row + 1][col + 1]))
+        possible_moves.push_back(position(row + 1, col + 1)); // next row next column -> bottom right
 
-    if (row + 1 <= maze_.size() && col + 1 <= maze_[row].size()) {
-        char cell = maze_[row + 1][col + 1]; // next row next column -> bottom right
-        if (cell != '*') {
-            position bottom_right(row + 1, col + 1);
-            possible_moves.push_back(bottom_right);
-        }
-    }
+    if (col - 1 >= 0 && cell_reachable(maze_[row][col - 1]))
+        possible_moves.push_back(position(row, col - 1)); // prev column same row -> left
 
-    if (col - 1 >= 0) {
-        char cell = maze_[row][col - 1]; // prev column same row -> left
-        if (cell != '*'){
-            position left(row, col - 1);
-            possible_moves.push_back(left);
-        }
-    }
+    if ((row - 1 >= 0 && col - 1 >= 0) && cell_reachable(maze_[row - 1][col - 1]))
+        possible_moves.push_back(position(row - 1, col - 1)); // prev row prev col -> top left
 
-    if (row - 1 >= 0 && col - 1 >= 0) {
-        char cell = maze_[row - 1][col - 1]; // prev row prev col -> top left 
-        if (cell != '*') {
-            position top_left(row - 1, col - 1);
-            possible_moves.push_back(top_left);
-        }
-    }
+    if ((row + 1 <= row_size_ && col - 1 >= 0) && cell_reachable(maze_[row + 1][col - 1]))
+        possible_moves.push_back(position(row + 1, col - 1)); // next row prev col -> bottom left
 
-    if (row + 1 <= maze_.size() && col - 1 >= 0) {
-        char cell = maze_[row + 1][col - 1]; // next row prev col -> bottom left
-        if (cell != '*') {
-            position bottom_left(row + 1, col - 1);
-            possible_moves.push_back(bottom_left);
-        }
-    }
+    if (row + 1 <= row_size_ && cell_reachable(maze_[row + 1][col]))
+        possible_moves.push_back(position(row + 1, col)); // same column next row -> down
 
-    if (row + 1 <= maze_.size()) {
-        char cell = maze_[row + 1][col]; // same column next row -> down
-        if (cell != '*') {
-            position down(row + 1, col);
-            possible_moves.push_back(down);
-        }
-    }
-
-    if (row - 1 >= 0) {
-        char cell = maze_[row - 1][col]; // same column prev row -> up
-        if (cell != '*') {
-            position up(row - 1, col);
-            possible_moves.push_back(up);
-        }
-    }
+    if (row - 1 >= 0 && cell_reachable(maze_[row - 1][col]))
+        possible_moves.push_back(position(row - 1, col)); // same column prev row -> up
 
     return possible_moves;
+}
+
+bool maze::cell_reachable(char cell) {
+    if (cell != '*') return true;
+    return false;
+}
+
+void maze::move(position p) {
+    std::vector<position> possible_moves = this->possible_moves();
+    if (std::find(possible_moves.begin(), possible_moves.end(), p) != possible_moves.end());
+        current_ = p;
 }
 
 void maze::print_maze() {
